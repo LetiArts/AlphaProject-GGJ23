917d158ecfb21e97ca6edae293c90909941eabef
BlackSentryDevXo <83230430+BlackSentryDevXo@users.noreply.github.com>
2023-02-05 00:34:20 +0000

    edits

--- Assets/Astrojack Refs/Scripts/UI/MenuManager.cs
+++ Assets/Astrojack Refs/Scripts/UI/MenuManager.cs
@@ -9,32 +9,32 @@ public class MenuManager : MonoBehaviour {
 	[SerializeField]
 	string PressButonSound = "ButtonPress";
 
-	SoundManager audioManager;
+	// SoundManager audioManager;
 
 	void Start()
 	{
-		audioManager = SoundManager.instance;
-		if (audioManager == null) 
-		{
-			Debug.LogError ("No audio found");
-		}
+		// audioManager = SoundManager.instance;
+		// if (audioManager == null) 
+		// {
+		// 	Debug.LogError ("No audio found");
+		// }
 	}
 	// Use this for initialization
 	public void StartGame () 
 	{
-		audioManager.PlaySFX (PressButonSound);
+		// audioManager.PlaySFX (PressButonSound);
 		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 	}
 
 	public void QuitGame()
 	{
-		audioManager.PlaySFX (PressButonSound);
+		// audioManager.PlaySFX (PressButonSound);
 		Debug.Log ("QUIT");
 		Application.Quit ();
 	}
 
 	public void OnMouseOver()
 	{
-		audioManager.PlaySFX (hoverOverSound);
+		// audioManager.PlaySFX (hoverOverSound);
 	}
 }
